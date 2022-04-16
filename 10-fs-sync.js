4c1ed6aadeef8efce37f58af0592360d63aa608e
glavanya <lavanyanaidu27@gmail.com>
2022-04-16 16:26:09 +0530

    added new files

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,8 +1,10 @@
 const {readFileSync, writeFileSync} = require('fs')
-
+console.log('Starting the task')
 const first = readFileSync('./content/first.txt','utf8')
 const second = readFileSync('./content/second.txt','utf8')
 
 writeFileSync('./content/result-sync.txt', `Here is the result file: ${first},${second}`,{ flag: 'a'}
 )
+console.log('Done with the task')
+console.log('starting the new one')
 
--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const {readFileSync, writeFileSync} = require('fs')
+//npm --version
+//local dependencies - use it only in this particular project
+//npm i <packagename>
+//global dependency - use it only in this particular project
+// npm install -g <packagename>
+//sudo  npm install -g <packagename> (mac)
+//package.json - manifest file(stores   important info about project/package)
+//manual approach(create package.json in the root , create properties etc)
+//npm init (step by step, press enter to skip)
 
-const first = readFileSync('./content/first.txt','utf8')
-const second = readFileSync('./content/second.txt','utf8')
-
-writeFileSync('./content/result-sync.txt', `Here is the result file: ${first},${second}`,{ flag: 'a'}
-)
-
-
+const _ = require('lodash')
+const items = [1,[2,[3,[4]]]]
+const newItems =_.flattenDeep(items)
+console.log(newItems);
